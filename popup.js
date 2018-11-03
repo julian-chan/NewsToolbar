be96cf2b4be94d0eddf46c096bf9ab68154d37d7
Joni Leung <jonileungck@berkeley.edu>
2018-11-03 11:44:51 -0700

    starter

--- popup.js
+++ popup.js
@@ -1,6 +1,3 @@
-//logging
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-console.log(firstHref);
 
 // code
-alert("Hello from your Chrome extension!")
+// alert("Hello newfrom your Chrome extension!")
